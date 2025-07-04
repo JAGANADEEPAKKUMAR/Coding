393c8931c6d031a98e132557a44de42efdba2ccf
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-04 12:13:00 +0530

    SquareOfANumber.py

---
+++ SquareOfANumber.py
@@ -0,0 +1,16 @@
+#Python
+num = int(input())
+print(num * num)
+
+#Java
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num * num);
+    }
+}
