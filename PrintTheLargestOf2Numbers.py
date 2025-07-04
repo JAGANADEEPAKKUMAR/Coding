6acb5023de5f761d483be9c27cae07052b5016ef
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-04 12:06:37 +0530

    PrintTheLargestOf2Numbers.py

---
+++ PrintTheLargestOf2Numbers.py
@@ -0,0 +1,25 @@
+#Python
+a,b = list(map(int,input().split()))
+if(a>b):
+    print(a)
+else:
+    print(b)
+  
+#Java
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a > b){
+            System.out.println(a);
+        }
+        else{
+            System.out.println(b);
+        }
+    }
+}
