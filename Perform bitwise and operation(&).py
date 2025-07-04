9378116abaa6c9285240b369b32bb83d3e408a29
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-04 18:54:31 +0530

    Perform bitwise and operation(&).py

---
+++ Perform bitwise and operation(&).py
@@ -0,0 +1,19 @@
+#Python
+
+a,b = list(map(int,input().split()))
+print(a & b)
+
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a & b);
+    }
+}
