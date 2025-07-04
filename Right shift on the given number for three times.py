b5a38c0925db6f8a896704d8e62181bd4dcfc8d8
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-04 19:10:36 +0530

    Right shift on the given number for three times.py

---
+++ Right shift on the given number for three times.py
@@ -0,0 +1,17 @@
+#Python
+
+a = int(input())
+print(a >> 3)
+
+#Java
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a >> 3);
+    }
+}
