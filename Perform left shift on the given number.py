23ef3f232e05f055642cb1db0eb24bcc6131c2c0
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-04 19:05:06 +0530

    Perform left shift on the given number.py

---
+++ Perform left shift on the given number.py
@@ -0,0 +1,18 @@
+#Python
+
+a = int(input())
+print(a << 1)
+
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a << 1);
+    }
+}
