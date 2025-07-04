fe25bc379e29735b4beb0d2c94b20ce6e6b3d7b4
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-04 18:36:38 +0530

    Check for at least one even number.py

---
+++ Check for at least one even number.py
@@ -0,0 +1,27 @@
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a % 2 == 0 || b % 2 == 0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
+
+#Python
+
+a,b = list(map(int,input().split()))
+if(a % 2 == 0 or b % 2 == 0):
+    print("Yes")
+else:
+    print("No")
