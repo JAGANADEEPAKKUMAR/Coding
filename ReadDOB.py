e06678d442e0a244aee70fdbfb5641d28048887f
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-04 10:38:23 +0530

    Create

---
+++ ReadDOB.py
@@ -0,0 +1,18 @@
+#Python
+
+dob = input()
+print("You were born on",dob)
+
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        String dob = sc.nextLine();
+        System.out.println("You were born on "+dob);
+    }
+}
