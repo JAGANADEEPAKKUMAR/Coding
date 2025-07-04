962372d3cd2646de88c68db5282ec9ec275a3486
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-04 18:44:05 +0530

    Check if both are equal.py

---
+++ Check if both are equal.py
@@ -0,0 +1,26 @@
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a == b){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+}
+    }
+}
+#Python
+
+a,b = list(map(int,input().split()))
+if(a == b):
+    print("Yes")
+else:
+    print("No")
