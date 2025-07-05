88307aa0eabb2b141a6a7e10ecdcc2235f0f37d0
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-05 08:06:06 +0530

    Convert to smaller to upper case.py

---
+++ Convert to smaller to upper case.py
@@ -0,0 +1,18 @@
+#Python
+
+letter = input()
+print(letter.upper())
+
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String letter = sc.nextLine();
+        System.out.println(letter.toUpperCase());
+    }
+}
