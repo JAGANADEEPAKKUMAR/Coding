cb7c5e3729f9ec0670358270de78fa03cddda099
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-05 08:00:54 +0530

    print the character equivalent of an integer.py

---
+++ print the character equivalent of an integer.py
@@ -0,0 +1,18 @@
+#Python
+
+ascii = int(input())
+print(chr(ascii))
+
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int ascii = sc.nextInt();
+        System.out.println((char)ascii);
+    }
+}
