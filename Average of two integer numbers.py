7561c32e3dc1eb8b135c17d85d9c9f69ad6908dc
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-06 15:18:08 +0530

    Average of two integer numbers.py

---
+++ Average of two integer numbers.py
@@ -0,0 +1,18 @@
+#Python
+
+
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double avg = (a+b)/2.0;
+        System.out.printf("%.2f",avg);
+    }
+}
