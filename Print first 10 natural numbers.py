18689defd21dd3fd108e610706bec5a79ec3a372
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-06 16:04:00 +0530

    Print first 10 natural numbers.py

---
+++ Print first 10 natural numbers.py
@@ -0,0 +1,19 @@
+#Python
+
+for i in range(1,11):
+    print(i,end = " ")
+  
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        for(int i = 1; i < 11; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
+
