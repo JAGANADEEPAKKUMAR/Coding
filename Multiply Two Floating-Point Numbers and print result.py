93b45b2cb0844010655ed042eb3c7114b532e35b
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-06 15:08:30 +0530

    Multiply Two Floating-Point Numbers and print result.py

--- Multiply Two Floating-Point Numbers and print result.py
+++ Multiply Two Floating-Point Numbers and print result.py
@@ -5,4 +5,15 @@ print(f"{(a*b):.2f}")
 
 #Java
 
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        System.out.printf("%.2f",(a*b));
+    }
+}
