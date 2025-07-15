e47032c8536d8459203fbee7b07d565d04641175
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-15 09:37:11 +0530

    Return the square of a number.py

---
+++ Return the square of a number.py
@@ -0,0 +1,18 @@
+#Python
+
+num = int(input())
+print(num ** 2)
+
+#Java
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    System.out.println(num * num);    
+    }
+}
