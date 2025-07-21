82320bd343464b779fc0203c220563b2d4773dc0
Deepak <159803190+JAGANADEEPAKKUMAR@users.noreply.github.com>
2025-07-21 10:46:55 +0530

    remove the occurrence of the word 'the' from the given string.py

--- remove the occurrence of the word 'the' from the given string.py
+++ remove the occurrence of the word 'the' from the given string.py
@@ -9,3 +9,29 @@ print(" ".join(sentence))
 
 #Java
 
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        
+        String[] words = line.split(" ");
+        ArrayList<String> sentence = new ArrayList<>(Arrays.asList(words));
+
+        for (int i = 0; i < sentence.size(); i++) {
+            if (sentence.get(i).equals("the")) {
+                sentence.remove(i);
+                break;
+            }
+        }
+
+        
+        for (int i = 0; i < sentence.size(); i++) {
+            System.out.print(sentence.get(i));
+            if (i != sentence.size() - 1) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
